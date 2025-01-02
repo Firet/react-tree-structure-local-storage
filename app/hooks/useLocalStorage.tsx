997bef641bd1bfa8ed52510e4ef5ff6df19fcd23
Firet <damianfederico@protonmail.com>
2025-01-02 18:16:15 -0300

    Fix build by avoid using setState conditionally

--- app/hooks/useLocalStorage.tsx
+++ app/hooks/useLocalStorage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 interface ILocalStorageItem<T> {
   key: string;
@@ -6,20 +6,31 @@ interface ILocalStorageItem<T> {
 }
 
 function useLocalStorage<T>(item: ILocalStorageItem<T>): [T, (newValue: T) => void] {
-  if (typeof window === 'undefined') {
-    return [item.initialValue, () => { }];
-  }
-
   const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === 'undefined') {
+      return item.initialValue;
+    }
+
     const storedItem = window.localStorage.getItem(item.key);
     return storedItem ? JSON.parse(storedItem) : item.initialValue;
   });
 
+  const setValue = useCallback(
+    (newValue: T) => {
+      if (typeof window !== 'undefined') {
+        setStoredValue(newValue);
+      }
+    },
+    []
+  );
+
   useEffect(() => {
-    window.localStorage.setItem(item.key, JSON.stringify(storedValue));
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem(item.key, JSON.stringify(storedValue));
+    }
   }, [storedValue, item.key]);
 
-  return [storedValue, setStoredValue];
+  return [storedValue, setValue];
 }
 
 export default useLocalStorage;
